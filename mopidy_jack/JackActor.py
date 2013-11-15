6373b004fa4f6feb16764ed9706999397a8445a8
Tue S. Dissing <tue@somersault.dk>
2013-11-15 12:37:19 +0100

    moving some code around.

--- mopidy_jack/JackActor.py
+++ mopidy_jack/JackActor.py
@@ -20,17 +20,22 @@ class JackActor(pykka.ThreadingActor, mopidy.core.CoreListener):
     def __init__(self):
         super(JackActor, self).__init__()
         
-    def on_start(self):
-        print "Attaching to jack."
-        jack.attach("default")
-        print "Attached."
-        print "Jack ports: %s", jack.get_ports()
+    #def on_start(self):
+        #print "Attaching to jack."
+        #jack.attach("default")
+        #print "Attached."
+        #print "Jack ports: %s", jack.get_ports()
 
     def on_stop(self):
         print "Detaching from JACK"
         jack.detach()
         
     def track_playback_started(self, tl_track):
+        print "Attaching to jack."
+        jack.attach("default")
+        print "Attached."
+        print "Jack ports: %s", jack.get_ports()
+        
         print "Track playback started. Time for some JACK magic."
         
         # Connect the specified JACK ports together.
