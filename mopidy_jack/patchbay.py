96ca5b959ce70c604e235a2670a45bd414463118
Tue S. Dissing <tue@somersault.dk>
2013-11-15 12:14:21 +0100

    getting properties back.

--- mopidy_jack/patchbay.py
+++ mopidy_jack/patchbay.py
@@ -73,24 +73,24 @@ def create_track(label, initial_volume, min_volume, max_volume,
             super(Track, self).__init__()
             self.volumes = (initial_volume,) * self.num_channels
 
-#        @gobject.property
-#        def label(self):
-#            return label
+        @gobject.property
+        def label(self):
+            return label
 
-#        @gobject.property
-#        def min_volume(self):
-#            return min_volume
+        @gobject.property
+        def min_volume(self):
+            return min_volume
 
-#        @gobject.property
-#        def max_volume(self):
-#            return max_volume
+        @gobject.property
+        def max_volume(self):
+            return max_volume
 
-#        @gobject.property
-#        def num_channels(self):
-#            return num_channels
+        @gobject.property
+        def num_channels(self):
+            return num_channels
 
-#        @gobject.property
-#        def flags(self):
-#            return flags
+        @gobject.property
+        def flags(self):
+            return flags
 
     return Track()
